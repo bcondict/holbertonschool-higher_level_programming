d24298d8dcdc311d54c957486b192a01342e5427
bcondict <jesus.junc10@gmail.com>
2022-05-02 23:48:31 -0500

    10-check_cycle.c test

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,12 +2,15 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *slow_p = list, *fast_p = list;
+	listint_t *slow_p, *fast_p;
+	
+	slow_p = list;
+	fast_p = list;
 
 	if (list == NULL)
 		return (0);
 
-	while (slow_p && fast_p && fast_p->next)
+	while (fast_p && fast_p->next)
 	{
 		slow_p = slow_p->next;
 		fast_p = fast_p->next->next;
