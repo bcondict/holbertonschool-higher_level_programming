dd9521b2839164d41aeeb801e8c208d28de41fd3
Jesus <jesus.junc10@gmail.com>
2022-05-05 19:30:08 -0500

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    
+    counter = 1
+    result = 0
+    while counter != len(argv):
+        result += int(argv[counter])
+        counter += 1
+    print("{}".format(result))
