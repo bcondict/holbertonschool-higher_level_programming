ae0b794a929d8d564635b75ae9681ff8c9aecbde
Jesus <jesus.junc10@gmail.com>
2022-05-05 20:10:46 -0500

    100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    import calculator_1
+
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        return 1
+    elif argv[3] != "+" or "-" or "*" or "/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        return 1
+    else:
+        a = int(argv[2])
+        b = int(argv[4])
+        if argv[3] == "+":
+            print("{} + {} = {}".format(argv[2], argv[4], add(a, b)))
+        elif argv[3] == "-":
+            print("{} - {} = {}".format(argv[2], argv[4], sub(a, b)))
+        elif argv[3] == "*":
+            print("{} * {} = {}".format(argv[2], argv[4], mul(a, b)))
+        else:
+            print("{} / {} = {}".format(argv[2], argv[4], div(a, b)))
