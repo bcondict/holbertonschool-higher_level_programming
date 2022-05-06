309a5268a2767029328bf856988067cb5999dfb5
Jesus <jesus.junc10@gmail.com>
2022-05-05 19:03:26 -0500

    2-args.py corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    
+
     from sys import argv
 
-    argv_count = len(argv) - 1
-    if argv_count == 0:
+    if len(argv) == 1:
         print("0 arguments.")
-    elif argv_count == 1:
-        print("{} argument:".format(argv_count, argv))
-        print("{}: {}".format(argv_count, argv[1]))
+    elif len(argv) == 2:
+        print("{} argument:".format(len(argv) - 1))
     else:
-        print("{} arguments:".format(len(argv), argv))
-        for i in range(1, argv_count + 1):
-            print("{}: {}".format(i, argv[i]))
+        print("{} arguments:".format(len(argv) - 1, argv))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
