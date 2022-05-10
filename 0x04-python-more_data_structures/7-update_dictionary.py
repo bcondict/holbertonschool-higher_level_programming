10c7b3a7e35ac5d1270c69250acb24a3a5814e02
Jesus <jesus.junc10@gmail.com>
2022-05-10 15:43:57 -0500

    7-update_dictionary.py correction

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,7 +2,7 @@
 def update_dictionary(a_dictionary, key, value):
     for i in a_dictionary:
         if i == key:
-            a_dictionary[i] = value
+            a_dictionary[key] = value
             return a_dictionary
         else:
             a_dictionary.update({key: value})
