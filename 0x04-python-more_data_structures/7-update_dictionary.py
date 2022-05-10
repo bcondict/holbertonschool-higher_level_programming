8aa6d20e229ade0906b9f0e8d246dee4fb8dfcd6
Jesus <jesus.junc10@gmail.com>
2022-05-10 15:39:30 -0500

    7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    for i in a_dictionary:
+        if i == key:
+            a_dictionary[i] = value
+            return a_dictionary
+        else:
+            a_dictionary.update({key: value})
+            return a_dictionary
