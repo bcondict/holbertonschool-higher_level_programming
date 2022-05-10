09d9cb703eebf258806c94f401976a7ca0b41662
Jesus <jesus.junc10@gmail.com>
2022-05-09 22:38:39 -0500

    9-max_integer.py correction

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == "":
+    if my_list == []:
         return None
     else:
         a = my_list[0]
@@ -8,4 +8,3 @@ def max_integer(my_list=[]):
             if my_list[i] > a:
                 a = my_list[i]
     return a
-                
