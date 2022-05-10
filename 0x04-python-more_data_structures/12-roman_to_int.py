b5e7bc207fd54a3c3e5e8e56aee3420ef5acbc9c
Jesus <jesus.junc10@gmail.com>
2022-05-10 18:45:02 -0500

    12-roman_to_int.py complete

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    letter = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if roman_string and type(roman_string) is str:
-        let = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        result = 0
+        num = 0
         for i in range(len(roman_string)):
-            for j in let:
-                if j == roman_string[i]:
-                    if i > 0 and let[j] > let[roman_string[i - 1]]:
-                        result += let[j] - let[roman_string[i - 1]] * 2
-                    else:
-                        result += let[j]
-    return result
+            if i > 0 and letter[roman_string[i]] > letter[roman_string[i-1]]:
+                num += letter[roman_string[i]] - letter[roman_string[i-1]] * 2
+            else:
+                num += letter[roman_string[i]]
+    return num
