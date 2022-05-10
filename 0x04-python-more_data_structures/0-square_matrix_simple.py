dc3a08b52ded9a634672060d0add5cba6b845590
Jesus <jesus.junc10@gmail.com>
2022-05-10 12:40:57 -0500

    0-square_matrix_simple.py style correction

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@ def square_matrix_simple(matrix=[]):
     square_value = []
     row_matrix = []
     for i in matrix:
-        for j in i: 
+        for j in i:
             row_matrix.append(j ** 2)
         square_value.append(row_matrix)
         row_matrix = []
