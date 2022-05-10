f73a36d91e7a319518ab188e55e6f9ceaa876780
Jesus <jesus.junc10@gmail.com>
2022-05-10 12:38:12 -0500

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    square_value = []
+    row_matrix = []
+    for i in matrix:
+        for j in i: 
+            row_matrix.append(j ** 2)
+        square_value.append(row_matrix)
+        row_matrix = []
+    return square_value
