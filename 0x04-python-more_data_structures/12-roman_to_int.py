115f8757103225cb4272674d9b58ca09b8f3aa8c
Jesus <jesus.junc10@gmail.com>
2022-05-10 18:29:40 -0500

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string and type(roman_string) is str:
+        letter = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        for i in range(len(roman_string)):
+            for j in letter:
+                if j == roman_string[i]:
+                    if i > 0 and letter[j] > letter[roman_string[i - 1]]:
+                        result += letter[j] - 3 + letter[roman_string[i - 1]]
+                    else: 
+                        result += letter[j]
+    return result
