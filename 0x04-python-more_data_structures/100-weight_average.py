5befea32f7d0bfb38675856dcd9ce87035d06eb0
Jesus <jesus.junc10@gmail.com>
2022-05-10 20:52:46 -0500

    100-weight_average.py correction

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if my_list:
+        mul = 0
+        div = 0
         for i in my_list:
             mul += i[0] * i[1]
             div += i[1]
