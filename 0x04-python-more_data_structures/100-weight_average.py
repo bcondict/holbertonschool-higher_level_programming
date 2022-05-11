920a29d4a81e17f16a51c7100348e92fbd50045e
Jesus <jesus.junc10@gmail.com>
2022-05-10 20:51:25 -0500

    100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list:
+        for i in my_list:
+            mul += i[0] * i[1]
+            div += i[1]
+        return mul/div
+    else:
+        return 0
