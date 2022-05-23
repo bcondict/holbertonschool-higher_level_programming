4d8a2381f9e228e89b82b9513d20a2d9f03ba598
bcondict <jesus.junc10@gmail.com>
2022-05-23 13:47:01 -0500

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,2 +1,8 @@
-#usr/bin/python3 2-safe_print_list_integers.py 3-safe_print_division.py 4-list_division.py 5-raise_exception.py 6-raise_exception_msg.py
-#usr/bin/python3
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return(True)
+    except (TypeError, ValueError):
+        return(False)
+
