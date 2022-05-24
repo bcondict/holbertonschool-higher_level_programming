6d1697e0f4f35d7e2f8d7457c86155b6f576442e
Jesus <jesus.junc10@gmail.com>
2022-05-24 16:03:32 -0500

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    __size = 0
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self._size = size
