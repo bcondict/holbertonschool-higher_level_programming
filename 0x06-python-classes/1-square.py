380b7a4ff9c1b4c6f8d03ceea12256e76edbf88b
Jesus <jesus.junc10@gmail.com>
2022-05-24 16:39:33 -0500

    1-square.py documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+""" class square to difine a square """
+
+
 class Square:
     __size = 0
 
+    """ define a squeare with 'size' size """
     def __init__(self, size):
         self.__size = size
