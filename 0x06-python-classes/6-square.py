82446c57e19639645dd70b956c3c7dbdda010c06
Jesus <jesus.junc10@gmail.com>
2022-05-24 23:43:31 -0500

    6-square.py test 22

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -12,7 +12,7 @@ class Square:
     @property
     def size(self):
         """ Size - Square's size """
-        return self._size
+        return self.__size
 
     @size.setter
     def size(self, value):
@@ -21,11 +21,11 @@ class Square:
         elif (value < 0):
             raise ValueError("size must be >= 0")
         else:
-            self._size = value
+            self.__size = value
 
     @property
     def position(self):
-        return self._position
+        return self.__position
 
     @position.setter
     def position(self, value):
@@ -35,22 +35,22 @@ class Square:
                 or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
-            self._position = value
+            self.__position = value
 
     """ define area of square """
     def area(self):
-        return (self.size ** 2)
+        return (self.__size ** 2)
 
     """ My print - Prints the square using # at given position """
     def my_print(self):
-        if (self.size == 0):
+        if (self.__size == 0):
             print()
         else:
-            for top_sapce in range(self.position[1]):
+            for top_sapce in range(self.__position[1]):
                 print()
-            for height in range(self.size):
-                for middle_sapce in range(self.position[0]):
+            for height in range(self.__size):
+                for middle_sapce in range(self.__position[0]):
                     print(" ", end="")
-                for width in range(self.size):
+                for width in range(self.__size):
                     print("#", end="")
                 print()
