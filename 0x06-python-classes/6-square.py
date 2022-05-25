2ab0ca88682b69c3b95b2f6de9b6dd17f44e4d7e
Jesus <jesus.junc10@gmail.com>
2022-05-24 20:53:36 -0500

    6-square.py taste

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,50 @@
+#\!/usr/bin/python3
+""" class square to difine a square """
+
+
+class Square:
+
+    """ define a squeare with 'size' size  and type and value error"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    """ define area of square """
+    def area(self):
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return(self.__position)
+
+    @position.setter
+    def position(self, value):
+        if len(self.position) > 1: 
+            raise TypeError("position must be a tuple of 2 positive integer")
+        else:
+            self.__position = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        for top_sapce in range(self.__position[1]):
+            print("")
+        for height in range(self.__size):
+            for middle_sapce in range(self.__position[0]):
+                print(" ", end="")
+            for width in range(self.__size):
+                print("#", end="")
+            print("")
