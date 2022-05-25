c80f9a6e345f109d62717cb052d1315247a96e2a
Jesus <jesus.junc10@gmail.com>
2022-05-24 23:02:50 -0500

    6-square.py test 11 alejo's way

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -38,16 +38,14 @@ class Square:
     """ define area of square """
     def area(self):
         return (self.__size ** 2)
-
     def my_print(self):
-        if (self.__size == 0):
-            print()
-        else:
-            for top_sapce in range(self.__position[1]):
-                print()
-            for height in range(self.__size):
-                for middle_sapce in range(self.__position[0]):
-                    print(" ", end="")
-                for width in range(self.__size):
-                    print("#", end="")
-                print()
+        """Print the square with the # character."""
+        if self.__size == 0:
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
