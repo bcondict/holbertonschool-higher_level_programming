fc92153f94fe2d590df7bc8fadde6085a4ef4263
Jesus <jesus.junc10@gmail.com>
2022-05-25 10:50:15 -0500

    6-square.py test 26

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,8 +6,8 @@ class Square:
 
     """ define a squeare with 'size' size  and type and value error"""
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
