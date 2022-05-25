c19b2027c52a139b0ca6ff5e3a7223c0739c8f0b
Jesus <jesus.junc10@gmail.com>
2022-05-24 23:40:13 -0500

    6-square.py test 19

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,25 +6,26 @@ class Square:
 
     """ define a squeare with 'size' size  and type and value error"""
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
         """ Size - Square's size """
-        return self.__size
+        return self._size
 
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        elif (value < 0):
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self._size = value
 
     @property
     def position(self):
-        return self.__position
+        return self._position
 
     @position.setter
     def position(self, value):
@@ -34,22 +35,22 @@ class Square:
                 or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
-            self.__position = value
+            self._position = value
 
     """ define area of square """
     def area(self):
-        return (self.__size ** 2)
+        return (self.size ** 2)
 
     """ My print - Prints the square using # at given position """
     def my_print(self):
-        if (self.__size == 0):
+        if (self.size == 0):
             print()
         else:
-            for top_sapce in range(self.__position[1]):
+            for top_sapce in range(self.position[1]):
                 print()
-            for height in range(self.__size):
-                for middle_sapce in range(self.__position[0]):
+            for height in range(self.size):
+                for middle_sapce in range(self.position[0]):
                     print(" ", end="")
-                for width in range(self.__size):
+                for width in range(self.size):
                     print("#", end="")
                 print()
