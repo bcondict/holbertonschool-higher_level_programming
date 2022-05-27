feedfc1a8a56e4ecae930d0ea1a4dcc5b63aa861
bcondict <jesus.junc10@gmail.com>
2022-05-26 19:40:06 -0500

    all files

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" function that divides all elements of a matrix"""
+
+
+def matrix_divided(matrix, div):
+    if (not isinstance(matrix, list) or matrix == [] or
+    not all(isinstance(row, list) for row in matrix) or
+    not all((isinstance(num, int) or isinstance(num, float))
+            for num in [height for row in matrix for height in row])):
+        raise TypeError("matrix must be a matrix \(list of lists) of"
+                        " integers/floats")
+    if not all(len(matrix[0]) == len(row) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    return [[round(i / div, 2) for i in j] for j in matrix]
