2c9025bab7c6290dfe97dff2e47b613d4cca56dc
Jesus <jesus.junc10@gmail.com>
2022-05-30 12:01:10 -0500

    all changes

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" print a square with the character #"""
+
+
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for height in range(size):
+        for lenght in range(size):
+            print("#", end="")
+        print()
---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" print a text with 2 new lines after ., ? or : character"""
+
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for n_t in range(len(text)):
+        less = text[n_t - 1]
+        if text[n_t] == '.' or text[n_t] == '?' or text[n_t] == ':' and n_t > 0:
+            if text[n_t] == " ":
+                continue
+            print(text[n_t])
+            print()
+        elif text[n_t] == ' ' and less == '.' or less == ':' or less == '?' and n_t > 0:
+            continue
+        else:
+            print(text[n_t], end="")
