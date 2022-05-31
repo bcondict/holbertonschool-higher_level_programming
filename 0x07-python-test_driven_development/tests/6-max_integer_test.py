7f5c35431730eeb64743fa5e91cadeee36af0e32
Jesus <jesus.junc10@gmail.com>
2022-05-31 01:10:56 -0500

    6-max_integer.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,20 +5,22 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
+    """Define unittests for max_integer([..])."""
+
     def test_normal(self):
-    self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
     def test_negative(self):
-    self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
 
     def test_empty(self):
-    self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([]), None)
 
     def test_zero(self):
-    self.assertEqual(max_integer([0]), 0)
+        self.assertEqual(max_integer([0]), 0)
 
     def test_float(self):
-    self.assertEqual(max_integer([4.3, 2.4, 5.6, 1.2]), 5.6)
+        self.assertEqual(max_integer([4.3, 2.4, 5.6, 1.2]), 5.6)
 
     def test_no_list(self):
         with self.assertRaises(TypeError):
@@ -29,10 +31,13 @@ class TestMaxInteger(unittest.TestCase):
             max_integer([1, 'two', 3])
 
     def test_max_lett(self):
-    self.assertEqual(max_integer(['a', 'b', 'c', 'd']), 'd')
+        self.assertEqual(max_integer(['a', 'b', 'c', 'd']), 'd')
 
     def test_max_string(self):
-    self.assertEqual(max_integer(['Hello', 'Good', 'Bye']), 'Hello')
+        self.assertEqual(max_integer(['Hello', 'Good', 'Bye']), 'Hello')
 
     def test_int_as_str(self):
-    self.assertEqual(max_integer(['1', '2', '3']), '3')
+        self.assertEqual(max_integer(['1', '2', '3']), '3')
+
+if __name__ == '__main__':
+    unittest.main()
