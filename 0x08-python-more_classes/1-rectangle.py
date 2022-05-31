0c4c169dff7010d5484c3248ba3b83c239c1be12
Jesus <jesus.junc10@gmail.com>
2022-05-31 18:33:29 -0500

    1-rectangle.py corrections

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-""" create a empty class that defines a rectangle"""
-
-
-from turtle import width
-from typing import Type
+""" create a class that defines a rectangle"""
 
 
 class Rectangle:
 
+    """defines rectangle with tidth and height"""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """width getter"""
         return self.width
 
     @width.setter
     def width(self, value):
+        """width setter"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -26,10 +25,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """height getter"""
         return self.height
 
     @height.setter
     def height(self, value):
+        """height setter"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
