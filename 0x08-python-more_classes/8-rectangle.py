2f4fb46a0d4323dc90ec235ea909fecf37c0abaf
Jesus <jesus.junc10@gmail.com>
2022-05-31 22:27:25 -0500

    8-rectangle.py correction

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -71,7 +71,7 @@ class Rectangle:
         print("Bye rectangle...")
 
     @staticmethod
-    def bigger_or_equal(cls, rect_1, rect_2):
+    def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
             raise TypeError("must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
