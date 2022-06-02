cf1ec54ef3f7f0cf82036c10d8ac67a17c7ef8d5
Jesus <jesus.junc10@gmail.com>
2022-06-01 23:43:15 -0500

    101-locked_class.py style correction

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,7 +3,7 @@
 
 
 class LockedClass:
-    """ 
+    """
     class or object attribute, that prevents the user
     from dynamically creating new instance attributes,
     except if the new instance attribute is called first_name.
