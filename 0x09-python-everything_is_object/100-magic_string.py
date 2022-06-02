21b291eac04bf9e55aea0dc2b0f23ddf7d743235
Jesus <jesus.junc10@gmail.com>
2022-06-01 23:36:22 -0500

    100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-count = 0
-def magic_string():
-    global count 
-    text_to_print = "BestSchool"
-    for i in range(count):
-        text_to_print += ", " + "BestSchool"
-    count += 1
-    return(text_to_print)
+def magic_string(buff=[]):
+    buff.extend(["BestSchool"])
+    return ", ".join(buff)
