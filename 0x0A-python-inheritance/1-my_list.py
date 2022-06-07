98d02ef76f9eb829033f6c6f47bb239616412d65
Jesus <jesus.junc10@gmail.com>
2022-06-07 18:53:30 -0500

    1-my_list.py comments

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""""""
+"""class inherits a list"""
 
 
 class MyList(list):
-    """"""
+    """ sort a given list"""
     def print_sorted(self):
         new_list = self.copy()
         new_list.sort()
