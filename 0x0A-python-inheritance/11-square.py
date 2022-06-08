5620afe5a05a32fceaf5fe5223a35f928a50cde8
Jesus <jesus.junc10@gmail.com>
2022-06-07 22:39:58 -0500

    11-square.py correction

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -14,4 +14,4 @@ class Square (Rectangle):
 
     def __str__(self):
         """set a Square description"""
-        return("[Square] {}/{}".format(self.__width, self.__height))
+        return("[Square] {}/{}".format(self.__size, self.__size))
