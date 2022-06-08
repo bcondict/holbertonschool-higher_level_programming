affbcc02e1ef935ba4d7211db3f3ce8541c3d38a
Jesus <jesus.junc10@gmail.com>
2022-06-07 21:31:48 -0500

    8-rectangle.py comments

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-""""""
+"""class inherits basegeometry"""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """validate if width or height are int and set it private"""
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("heigh", height)
