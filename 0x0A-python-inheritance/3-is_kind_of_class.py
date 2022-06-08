d4ad105e0efafc9ef478c1b54dfd02e05b0f2565
Jesus <jesus.junc10@gmail.com>
2022-06-07 19:48:27 -0500

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" see if an object is an specific istance"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ return true if obj is a class or false if is not """
+    return (isinstance(obj, a_class))
