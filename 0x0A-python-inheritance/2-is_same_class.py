40c33bad9ab41d7c3870534956be9b2254fd8cf1
Jesus <jesus.junc10@gmail.com>
2022-06-07 19:43:36 -0500

    2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""" """
+""" see if an object is an specific type"""
 
 
 def is_same_class(obj, a_class):
+    """ return true if obj is a class or false if is not """
     if type(obj) is a_class:
         return True
     return False
