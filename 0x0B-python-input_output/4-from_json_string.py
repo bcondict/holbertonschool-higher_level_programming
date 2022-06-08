1dcdc9ab0e0363222adfc9769c36f18084229143
Jesus <jesus.junc10@gmail.com>
2022-06-08 18:28:32 -0500

    4-from_json_string.py correction

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-""" """
+"""function that return an object represented by a JSON string """
 
 
 import json
 
 
 def from_json_string(my_str):
+    """object by a json string"""
     new_str = json.dumps(my_str)
     return(new_str)
