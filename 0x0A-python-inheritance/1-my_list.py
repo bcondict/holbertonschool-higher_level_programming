7146e50b89177107c91d48f9387fce8c6d1d7a0e
Jesus <jesus.junc10@gmail.com>
2022-06-07 20:39:03 -0500

    1-my_list.py complete

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,5 @@
 class MyList(list):
     """ sort a given list"""
     def print_sorted(self):
-        new_list = self.copy()
-        new_list.sort()
-        print(new_list)
+        """print sorted list"""
+        print(sorted(self))
