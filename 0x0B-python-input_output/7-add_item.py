d0fa335033d5709182f98fd6328798e16431bcb4
Jesus <jesus.junc10@gmail.com>
2022-06-08 20:59:39 -0500

    7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""script that add all arguments to a python list and save it in a file"""
+
+
+from sys import argv
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    new_list = load_from_json_file(filename)
+except Exception:
+    new_list = []
+save_to_json_file(new_list + argv[1:], filename)
