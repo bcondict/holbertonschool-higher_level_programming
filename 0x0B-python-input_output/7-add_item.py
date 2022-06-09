3b24e2d6e254cba5f4b66042c73a93bb4a654f86
Jesus <jesus.junc10@gmail.com>
2022-06-08 21:01:39 -0500

    7-add_item.py comments correction

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """script that add all arguments to a python list and save it in a file"""
 
-
 from sys import argv
-
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
