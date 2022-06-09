70ef122776e2a6cf49943c212d3d34e9c510af4b
Jesus <jesus.junc10@gmail.com>
2022-06-08 19:56:50 -0500

    6-load_from_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-function that writes an object to a text file
+function that writes an object to a text file 
 using JSON representation
 """
 
@@ -11,5 +11,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """writes an object to a text file"""
     with open(filename, 'w', encoding="utf-8") as f:
-        new_str = json.dumps(my_obj)
+        new_str = json.loads(my_obj)
         return f.write(new_str)
---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function that creates an object from a JSON file"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """creates an objet from a json file"""
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.load(f)
