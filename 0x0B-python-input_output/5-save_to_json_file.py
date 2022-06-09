4cd70a88b71908e30cebb90dbf18e7ed94fcea28
Jesus <jesus.junc10@gmail.com>
2022-06-08 21:08:06 -0500

    7-add_item.py test 2

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -11,5 +11,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """writes an object to a text file"""
     with open(filename, 'w', encoding="utf-8") as f:
-        new_str = json.loads(my_obj)
-        return f.write(new_str)
+        return json.dump(my_obj, f)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """script that add all arguments to a python list and save it in a file"""
 
-import sys
+from sys import argv
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
@@ -12,4 +12,4 @@ try:
     new_list = load_from_json_file(filename)
 except Exception:
     new_list = []
-save_to_json_file(new_list + sys.argv[1:], filename)
+save_to_json_file(new_list + argv[1:], filename)
