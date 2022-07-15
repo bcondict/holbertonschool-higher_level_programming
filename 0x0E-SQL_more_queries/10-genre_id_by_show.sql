69c5d0a874d41abb9963fab25d93dc3288768b73
Jesus <jesus.junc10@gmail.com>
2022-07-14 21:42:27 -0500

    10-genre_id_by_show.sql test

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- lists all show contained in a datebase that have at least one genre linked
-SELECT tv_shows.title, tv_shows.genre_id FROM tv_shows
-INNER JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
 WHERE tv_shows_genres.genre_id IS NOT NULL
-ORDER BY tv_shows.title ASC, tv_shows_genres.genre_id ASC;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
