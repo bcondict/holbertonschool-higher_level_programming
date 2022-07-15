f7b758fbec7ae77f8d2e93fdf8c1091d7dba8615
Jesus <jesus.junc10@gmail.com>
2022-07-14 22:43:38 -0500

    15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,6 @@
 -- lists all Comedy shows in a datebase
 SELECT tv_shows.title FROM tv_shows
-LEFT JOIN tv_shows_genres ON tv_shows_genres.show_id = tv_shows.id
-LEFT JOIN tv_genres ON tv_genres.id = tv_shows_genres.genre_id
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title;
