8d08338a54015640a9e25af3d3e20ee3df84f751
Jesus <jesus.junc10@gmail.com>
2022-07-14 22:18:15 -0500

    solvin problems

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     PRIMARY KEY(id),
     id INT UNIQUE NOT NULL AUTO_INCREMENT,
     state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
     FOREIGN KEY (state_id) REFERENCES states(id)
+    name VARCHAR(256) NOT NULL,
 );
