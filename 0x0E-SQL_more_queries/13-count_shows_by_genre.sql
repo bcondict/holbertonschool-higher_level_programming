916a4fbb7d1ae7fbb754eaa15f10099d0a76c0dd
Jesus <jesus.junc10@gmail.com>
2022-07-14 22:29:37 -0500

    13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all shows contained in a datebase without a genre linked
-SELECT tv_genres.name, COUNT(tv_shows_genres.genre_id) AS MY_COUNT FROM tv_shows_genres
-RIGHT JOIN tv_genres ON tv_genres.id = tv_shows_genres.genre_id
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS MY_COUNT
+RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
 ORDER BY MY_COUNT DESC;
