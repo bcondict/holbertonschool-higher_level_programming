72b2f772a1e79890213f32d8e6b2e889b44e4b18
Jesus <jesus.junc10@gmail.com>
2022-07-14 22:11:26 -0500

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,9 @@
 -- create a database and a table (inside database)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-    id INT UNIQUE AUTO_INCREMENT NOT NULL ,
+    PRIMARY KEY(id),
+    id INT UNIQUE NOT NULL AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (state_id) REFERENCES state(id)
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
