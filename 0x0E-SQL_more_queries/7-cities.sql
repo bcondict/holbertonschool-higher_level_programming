1c08937d3ad9a0a0b5216e9c095c058bd19c4467
Jesus <jesus.junc10@gmail.com>
2022-07-14 21:27:12 -0500

    7-cities.sql test 2

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT UNIQUE AUTO_INCREMENT NOT NULL ,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(state_id) REFERENCES state(id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES state(id)
 );
