02587273b20154ed28ee3d940436414a433a4a9f
Jesus <jesus.junc10@gmail.com>
2022-07-14 22:49:24 -0500

    16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- script that lists all Comedy shows in the database hbtn_0d_tvshows.
 SELECT tv_shows.title, tv_genres.name FROM tv_shows
-LEFT JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id
-LEFT JOIN tv_genres ON tv_shows_genres.genre_id = tv_genres.id 
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title, tv_genres.name;
