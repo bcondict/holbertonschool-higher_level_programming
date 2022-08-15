4102cd5d64ed73c26919b345437e286edf167132
bcondict <jesus.junc10@gmail.com>
2022-08-15 18:54:13 -0500

    [OK] correct message

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,12 +2,8 @@
 
 // print a message depending of the number of arguments passed
 
-const argc = process.argv.lenght;
+const argc = process.argv.length;
 
-if (argc < 3) {
-  console.log('No argument');
-} else if (argc === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
+if (argc < 3) console.log('No argument')
+else if (argc === 3) console.log('Argument found')
+else console.log('Arguments found')
