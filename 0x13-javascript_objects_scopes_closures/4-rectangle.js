a552ecd60ad4a8464f0384e19743be5dfc172cb8
Jesus <jesus.junc10@gmail.com>
2022-08-16 10:56:18 -0500

    [OK] 4 task correct output

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    for (var i = 0; i < this.height; i++){
+      console.log('X'.repeat(this.width));
+    }
+  }
+  rotate() {
+    var temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+
+module.exports = Rectangle;
