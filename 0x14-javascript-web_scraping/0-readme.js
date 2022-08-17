a0e9b4ee81b76a26cf3c057fc0d13865ba06057f
bcondict <jesus.junc10@gmail.com>
2022-08-17 12:21:59 -0500

    [OK] 0 task correct output

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node 
+
+const fs = require("fs");
+
+fs.readFile(`${process.argv[2]}`, (error, data) => {
+    if (error) {
+        throw error;
+    }
+    console.log(data.toString());
+});
