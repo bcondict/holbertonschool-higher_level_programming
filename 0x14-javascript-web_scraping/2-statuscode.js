70fe8800e2b402057a75e9263f2ce0c9d3febabf
bcondict <jesus.junc10@gmail.com>
2022-08-17 14:44:36 -0500

    [OK] 2 task test

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response) {
+	if (err) console.log(err);
+	console.log(`code: ${response.statusCode}`)
+})
