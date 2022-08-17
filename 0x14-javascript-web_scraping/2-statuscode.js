9566d0eabb726791e34cb097dded0c2dad886aad
bcondict <jesus.junc10@gmail.com>
2022-08-17 14:47:25 -0500

    [OK] 2 task correct output

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,6 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (err, response) {
-	if (err) console.log(err);
-	console.log(`code: ${response.statusCode}`)
-})
+  if (err) console.log(err);
+  console.log(`code: ${response.statusCode}`);
+});
