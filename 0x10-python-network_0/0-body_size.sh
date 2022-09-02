36c94cea709befd3d48234ccc9f82f77b22a5cc5
Jesus <jesus.junc10@gmail.com>
2022-09-01 20:19:08 -0500

    change single quotes by two

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 # takes in a URL, sends a request to that URL and displays the size of the body 
 
-curl -sI '$1' | grep 'Content-Lenght' | cut -d' ' -f 2
+curl -sI "$1" | grep 'Content-Lenght' | cut -d' ' -f 2
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 #  takes in a URL, sends a GET request to the URL, and displays the body of the response
 
-curl -sI '$1'
+curl -sI "$1"
