50e22cfd31f0f4cef4ed4825f069e4596ddf1d6a
Jesus <jesus.junc10@gmail.com>
2022-09-25 16:01:39 -0500

    0 task correction

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL and displays the size of the body 
-curl -sI "$1" | grep 'Content-Lenght' | cut -d' ' -f 2
+curl -sI {$1} | grep Content-Lenght | cut -d' ' -f 2
